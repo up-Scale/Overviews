35ccf7e62160cd55eb02f6e20c6de8def6f196e7
Inderjeet Kaur <inderjeet.kchawla@gmail.com>
2018-10-31 19:56:30 -0400

    Cleaned up all the extra code

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var SRC_DIR = path.join(__dirname, '/react-client/src');
 var DIST_DIR = path.join(__dirname, '/react-client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.js`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'overview.bundle.js',
     path: DIST_DIR
